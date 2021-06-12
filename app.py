c873fbd27ed726bb263e6a8ad6134e3be981d908
analystwiz <subranjit.sahoo@bytelearn.ai>
2021-06-12 10:34:29 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.title("Welcome to twilight")
+
+st.write("Twilight is a python package to work with text data efficiently.It empowers non technical marketing folks with a tool to quickly understand any text-based dataset and it provides a intuitive ui to explore insights from text.")
+
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)

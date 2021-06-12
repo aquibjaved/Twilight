aec9532a092c0a0a36bb769f5ff24bbb18ffa86e
analystwiz <subranjit.sahoo@bytelearn.ai>
2021-06-12 10:55:19 +0530

    added side menu and file upload

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ st.write(
     "Twilight is a python package to work with text data efficiently.It empowers non technical marketing folks with a tool to quickly understand any text-based dataset and it provides a intuitive ui to explore insights from text."
 )
 
-# x = st.slider('x')  # 👈 this is a widget
-# st.write(x, 'squared is', x * x)
+st.subheader("Document Files")
+docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
+
+def main():
+	menu = ["Home","Dataset","Document Files","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+    # if choice == "Document Files":
+	# 	st.subheader("Document Files")
+	# 	docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
+
+if __name__ == '__main__':
+	main()

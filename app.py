2037d5881ccce223faf0fabecde874b27011d950
analystwiz <subranjit.sahoo@bytelearn.ai>
2021-06-16 23:14:19 +0530

    configurable wordcloud

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+from twilight import basic_eda
+import pandas as pd
 
 st.title("Welcome to twilight")
 
@@ -7,11 +8,25 @@ st.write(
     "Twilight is a python package to work with text data efficiently. It's a no code tool to quickly understand any text-based document and it provides an intuitive UI to explore insights from text."
 )
 
-st.subheader("Document Files")
-docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
+st.subheader("Most frequent words from sample text file")
+# docx_file = st.file_uploader("Load Sample File",type=['txt','docx','pdf'])
+# if docx_file:
+wc_g= st.button("Generate Wordcloud")
+if wc_g:
+	data_load_state = st.text('Loading data...')
+	df = pd.read_csv('./data/qa_dataset.csv')
+	data_load_state = st.text('Data Loaded!')
+	wd = basic_eda.GenerateWordCloud(dataframe=df, column_name='question')
+	x=st.slider("words",50,250)
+	wd_obj = wd.get_word_cloud(max_words=x)
+	try:
+		st.image(wd.show_word_cloud(wordcloud_object=wd_obj).show())
+	except:
+		st.image("questions_wordcloud.png")
+	
 
 def main():
-	menu = ["Home","Dataset","Document Files","About"]
+	menu = ["Home","Dataset","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
     # if choice == "Document Files":
 	# 	st.subheader("Document Files")
--- twilight/basic_eda.py
+++ twilight/basic_eda.py
@@ -11,28 +11,35 @@ class GenerateWordCloud:
         self.column_name = column_name
 
 
-    def get_word_cloud(self):
+    def get_word_cloud(self,max_words):
         '''
         this func, select the column name and get all the text
         later convert all that to text to generate wordcloud object
         :return:
         '''
         text_from_column =  ' '.join(self.dataframe[self.column_name].values.tolist())
-        wordcloud = WordCloud().generate(text_from_column)
+        wordcloud = WordCloud(max_words=max_words).generate(text_from_column)
         return wordcloud
 
     def show_word_cloud(self, wordcloud_object):
         # Display the generated image:
-        plt.imshow(wordcloud_object, interpolation='bilinear')
-        plt.axis("off")
-        plt.show()
+        x= plt.imshow(wordcloud_object, interpolation='bilinear')
+        x.axis("off")
+        # x.show()
+        return x
+
+    # def controller(self):
+
+    #     df = pd.read_csv('../data/qa_dataset.csv')
+    #     wd = GenerateWordCloud(dataframe=df, column_name='question')
+    #     wd_obj = wd.get_word_cloud()
+    #     wd.show_word_cloud(wordcloud_object=wd_obj)
 
 
 if __name__  == '__main__':
 
     # Read data
     df = pd.read_csv('../data/qa_dataset.csv')
-
     wd = GenerateWordCloud(dataframe=df, column_name='question')
     wd_obj = wd.get_word_cloud()
     wd.show_word_cloud(wordcloud_object=wd_obj)

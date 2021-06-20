190ebe04176d4e85d237bacb592c82d74950fd78
analystwiz <subranjit.sahoo@bytelearn.ai>
2021-06-20 21:24:01 +0530

    Added Menuu options and Quick Demo

--- app.py
+++ app.py
@@ -2,22 +2,24 @@ import streamlit as st
 from twilight import basic_eda
 import pandas as pd
 
-st.title("Welcome to twilight")
+st.title("Welcome to Twilight")
 
 st.write(
     "Twilight is a python package to work with text data efficiently. It's a no code tool to quickly understand any text-based document and it provides an intuitive UI to explore insights from text."
 )
 
-st.subheader("Most frequent words from sample text file")
-# docx_file = st.file_uploader("Load Sample File",type=['txt','docx','pdf'])
+
+docx_file = st.file_uploader("Load Sample File",type=['txt'])#,'docx','pdf'])
 # if docx_file:
-wc_g= st.button("Generate Wordcloud")
-if wc_g:
+st.title("Check out a Quick Demo")
+sd= st.button("Load Sample Dataset")
+if sd:
 	data_load_state = st.text('Loading data...')
 	df = pd.read_csv('./data/qa_dataset.csv')
 	data_load_state = st.text('Data Loaded!')
+	x= st.number_input("Enter no of words",value=10)
+	st.subheader("Most frequent words from sample text file")
 	wd = basic_eda.GenerateWordCloud(dataframe=df, column_name='question')
-	x=st.slider("words",50,250)
 	wd_obj = wd.get_word_cloud(max_words=x)
 	try:
 		st.image(wd.show_word_cloud(wordcloud_object=wd_obj).show())
@@ -25,12 +27,12 @@ if wc_g:
 		st.image("questions_wordcloud.png")
 	
 
-def main():
-	menu = ["Home","Dataset","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+# def main():
+menu = ["N-Grams","Entity Extraction","Topic Modelling","Question Anwering","About"]
+choice = st.sidebar.selectbox("NLP Features",menu)
     # if choice == "Document Files":
 	# 	st.subheader("Document Files")
 	# 	docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
 
-if __name__ == '__main__':
-	main()
+# if __name__ == '__main__':
+# 	main()

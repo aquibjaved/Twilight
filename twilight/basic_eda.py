41f5c5ed7bbb08fed3c081d6b716323ffc666495
Subranjit sahoo <subhranjit93@gmail.com>
2021-06-23 11:31:41 +0530

    Features/wordcloud (#4)
    * configurable wordcloud
    
    * added more extensions
    
    * Added Menuu options and Quick Demo
    
    * bug fixes for wordcloud
    
    * added csv in file upload
    
    * added functionality for selecting columns
    
    * added button to generate wordcloud
    
    * added tsv to file upload type list
    
    * bug fixes
    
    * added multiselect dropdown for selecting columns
    
    Co-authored-by: analystwiz <subranjit.sahoo@bytelearn.ai>

--- twilight/basic_eda.py
+++ twilight/basic_eda.py
@@ -11,14 +11,14 @@ class GenerateWordCloud:
         self.column_name = column_name
 
 
-    def get_word_cloud(self):
+    def get_word_cloud(self,max_words):
         '''
         this func, select the column name and get all the text
         later convert all that to text to generate wordcloud object
         :return:
         '''
         text_from_column =  ' '.join(self.dataframe[self.column_name].values.tolist())
-        wordcloud = WordCloud().generate(text_from_column)
+        wordcloud = WordCloud(max_words=max_words).generate(text_from_column)
         return wordcloud
 
     def show_word_cloud(self, wordcloud_object):
@@ -26,13 +26,13 @@ class GenerateWordCloud:
         plt.imshow(wordcloud_object, interpolation='bilinear')
         plt.axis("off")
         plt.show()
+        # return x
 
 
 if __name__  == '__main__':
 
     # Read data
     df = pd.read_csv('../data/qa_dataset.csv')
-
     wd = GenerateWordCloud(dataframe=df, column_name='question')
     wd_obj = wd.get_word_cloud()
     wd.show_word_cloud(wordcloud_object=wd_obj)

1fd4e407d41d0449c9c7da4b0ac23fd4bb76d1a0
aquibjaved <ajkhan357@gmail.com>
2021-07-10 14:13:35 +0200

    loose code for lda

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from twilight import basic_eda,file_parsing
+from twilight import basic_eda, file_parsing
+from twilight.basic_eda import Features
 
 st.title("Welcome to Twilight")
 
@@ -8,27 +9,36 @@ st.write(
 )
 
 #* Functionality for file upload & column selection
-uploaded_file = st.file_uploader("Load Sample File",type=['txt','csv','tsv','xlsx'])#,'docx','pdf'])
+uploaded_file = st.file_uploader("Load Sample File",type=['txt','csv','tsv','xlsx'])
 
 if uploaded_file:
 	df= file_parsing.get_file_obj(uploaded_file=uploaded_file)
 	st.write(df)
-	_cols = list(df.columns.values)
+	_cols = df.columns.values.tolist()
 	col1, col2 =st.beta_columns(2)
 	with col1:
 		_col_selected = st.multiselect("Select column to analyze",_cols)
 
 	if _col_selected:
-		# with st.beta_container():
 		with col2:
-			x= st.number_input("# frequent words", value=100)
+			x = st.number_input("# frequent words", value=100)
+			y = st.number_input("# of Topics", value=5)
+
 		if x:
 			st.button("Generate Word Cloud")
 			wd = basic_eda.GenerateWordCloud(dataframe=df, column_name=_col_selected[0])
 			wd_obj = wd.get_word_cloud(max_words=x)
 			st.write(wd.show_word_cloud(wordcloud_object=wd_obj))
 
-	
+		if y:
+			st.button("Get Topics 📚")
+			data_collected = df[_col_selected[0]].values.tolist()
+			feat = Features(data=data_collected, num_topics=10)
+			df_topics = feat.get_topics()
+			st.write(df_topics)
+
+
+
 st.title("Check out a Quick Demo")
 sd= st.button("Load Sample Dataset")
 if sd:

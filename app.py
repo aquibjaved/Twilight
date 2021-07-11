a47d437c3d7e58d1b488bc31218ac30f69335b30
analystwiz <subranjit.sahoo@bytelearn.ai>
2021-07-11 15:45:27 +0530

    Modified App layout for LDA & wordcloud

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from twilight import basic_eda, file_parsing
 from twilight.basic_eda import Features
+import pandas as pd
 
+# st.beta_set_page_config(layout="wide")
 st.title("Welcome to Twilight")
 
 st.write(
@@ -15,29 +17,25 @@ if uploaded_file:
 	df= file_parsing.get_file_obj(uploaded_file=uploaded_file)
 	st.write(df)
 	_cols = df.columns.values.tolist()
-	col1, col2 =st.beta_columns(2)
-	with col1:
-		_col_selected = st.multiselect("Select column to analyze",_cols)
-
+	# col1, col2 = st.beta_columns(2)
+	# with col1:
+	_col_selected = st.multiselect("Select column to analyze",_cols)
 	if _col_selected:
-		with col2:
-			x = st.number_input("# frequent words", value=100)
-			y = st.number_input("# of Topics", value=5)
-
-		if x:
-			st.button("Generate Word Cloud")
+		st.header("Most Frequent Words 📊")
+		my_expander1 = st.beta_expander(label="Word Cloud")
+		st.header("Get Topics 📚")
+		my_expander2= st.beta_expander(label= "Discover Topics")
+		with my_expander1:
+			x = st.number_input("Enter top-n most frequent words", value=100)
 			wd = basic_eda.GenerateWordCloud(dataframe=df, column_name=_col_selected[0])
 			wd_obj = wd.get_word_cloud(max_words=x)
 			st.write(wd.show_word_cloud(wordcloud_object=wd_obj))
-
-		if y:
-			st.button("Get Topics 📚")
+		with my_expander2:
+			x = st.number_input("Enter no of topics", value=5)
 			data_collected = df[_col_selected[0]].values.tolist()
-			feat = Features(data=data_collected, num_topics=10)
+			feat = Features(data=data_collected, num_topics=x)
 			df_topics = feat.get_topics()
 			st.write(df_topics)
-
-
 
 st.title("Check out a Quick Demo")
 sd= st.button("Load Sample Dataset")

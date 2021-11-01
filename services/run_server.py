ba728001850ecfe367beec974486913bb90b3767
aquib <ajkhan357@gmail.com>
2021-11-01 10:25:18 +0100

    updated loose topics api

--- services/run_server.py
+++ services/run_server.py
@@ -6,7 +6,7 @@ import pandas as pd
 import sys
 sys.path.append('../')
 from twilight.basic_eda import GenerateWordCloud
-
+from twilight.basic_eda import Features
 app = FastAPI()
 
 origins = ["*"]
@@ -27,6 +27,8 @@ class GetCloud(BaseModel):
     n_count: int
     column_name: str
 
+class GetTopics(BaseModel):
+    n_topics: int
 
 @app.post("/")
 async def root(arbitrary_json: JSONStructure = None):
@@ -37,6 +39,15 @@ async def root(arbitrary_json: JSONStructure = None):
 
 @app.post("/get_word_cloud/")
 async def word_cloud(data: GetCloud):
+    global text_corpus
+    text_corpus = df[data.column_name].values.tolist()
     object_wc = GenerateWordCloud(dataframe=df, column_name=data.column_name, max_words=data.n_count)
     res = object_wc.get_word_cloud()
     return res
+
+
+@app.post("/get_topics/",response_model=List)
+async def get_topic(data: GetTopics):
+    #text_corpus = df.col_name.values.tolist()
+    feat = Features(data=text_corpus, num_topics=data.n_topics)
+    return feat.get_topics()
